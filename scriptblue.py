3cf2935eef1a205d9bd2b26de94570867dcf438d
Aryan Kashyap <142740897+RockstarAryan@users.noreply.github.com>
2024-09-30 05:45:15 +0530

    Add files via upload

---
+++ scriptblue.py
@@ -0,0 +1,10 @@
+#Script for the teams - 
+# 0 - Rock
+#1 is scissors
+#2 is paper
+
+import random
+def script():
+    return random.randint(0,3)
+
+script()
---
+++ scriptred.py
@@ -0,0 +1,10 @@
+#Script for the teams - 
+# 0 - Rock
+#1 is scissors
+#2 is paper
+
+
+def script():
+    return 1
+
+script()
